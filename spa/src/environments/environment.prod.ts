fb49073d0316b2599715c1bda84d09ebf7017e96
Shinto C V <cshintov@gmail.com>
2021-03-12 16:34:14 +0530

    Configure spa for production

--- spa/src/environments/environment.prod.ts
+++ spa/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  bffURL: "http://localhost:8080/",
-  gatewayURL: "http://localhost:8888/",
-  patdemoURL: "http://localhost:8081/",
-  patinsURL: "http://localhost:8082/",
+  bffURL: "http://bff:8080/",
+  gatewayURL: "http://gateway:8888/",
+  patdemoURL: "http://patdemo:8081/",
+  patinsURL: "http://patins:8082/",
 };
